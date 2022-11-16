32ba5cc854da5d3aebf4ccc2a991d0596c293550
ThuDepGai <thule210903@gmail.com>
2022-11-16 13:48:22 +0700

    first commit

---
+++ app/Models/Payment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Payment extends Model
+{
+   public $timestamps = false;
+   protected $fillable = [
+    'payment_method' ,  'payment_status' ,
+   ]; 
+   protected $primaryKey =  'payment_id'; /* Khóa Chính */
+   protected $table =   'tbl_payment'; /* Tên Bảng */
+}
